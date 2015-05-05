bab4b0819a6ae412f2b39f6df465aec5620002f8
John Frederick Lake <jlake2@fitz94.helios.nd.edu>
2015-05-05 12:17:22 -0400

    Added moving platforms and you can only jump once.

--- playerdir/serverPlayer.py
+++ playerdir/serverPlayer.py
@@ -2,6 +2,8 @@
 #Final Project
 #Shooter Platforming Game
 
+#This is the file that should be run first.  This is the "server" player, who is red. 
+
 import pygame
 import math
 import sys
@@ -17,16 +19,15 @@ import cPickle as pickle
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 
-
-
 #Data Protocol
 class Data(LineReceiver):
-	#Forward data to client connection
+	#Now we can send data:
 	def connectionMade(self):
 		gs.areConnected()
+
+	#Get and interpret data sent from the other player: 
 	def dataReceived(self,data):
 		d = pickle.loads(data)	
-
 		if type(d) is pygame.Rect:
 			gs.moveEnemy(d)
 		else:
@@ -36,7 +37,7 @@ class Data(LineReceiver):
 				gs.deleteBullet(d['b_x'])
 			elif d['message'] == "deletePlayer":
 				gs.deleteEnemy()
-		
+
 	#Send data along data connection
 	def sendData(self,arg):
 		self.sendLine(arg)
@@ -68,20 +69,23 @@ class Bullet(pygame.sprite.Sprite):
 			self.velX = -7
 	def tick(self):
 		self.rect = self.rect.move(self.velX,0)
-		#code to kill bullet once off screen ... syntax not working
+		#code to kill bullet once off screen
 		if (self.rect.right > 1000 or self.rect.left < -1000 or self.rect.top > 1000 or self.rect.bottom < -1000):
 			del self
 
 class Wall(pygame.sprite.Sprite):
+	#We use types of walls to determine collision results
 	#left = 1
 	#top = 2
 	#right = 3
 	#down = 4
-	#plat = 5
-	def __init__(self, gs=None, x_pos=0, y_pos=0,type=5):
+	#platform = 5
+	def __init__(self, gs=None, x_pos=0, y_pos=0,type=5,dir=0):
 		#Initialize the sprite, sound, and images:
 		gs.wall = self
 		self.type = type
+		self.dir = dir
+		self.yPos = y_pos
 		pygame.sprite.Sprite.__init__(self)
 		try:
 			self.image = pygame.image.load("images/wall.png")
@@ -91,11 +95,24 @@ class Wall(pygame.sprite.Sprite):
 			exit(0)
 		self.rect = self.image.get_rect()
 		self.rect = self.rect.move(x_pos, y_pos)
+	def tick(self):
+		if self.type == 5:
+			#move back and forth:
+			if self.dir:
+				#move left:
+				self.rect = self.rect.move(1,0)
+			else:
+				#move right:
+				self.rect = self.rect.move(-1,0)
+			if self.rect.right >= SCREEN_WIDTH-self.rect.width:
+				self.dir = 0
+			elif self.rect.left == self.rect.width:
+				self.dir = 1
 
 #Player class: 
 class Player(pygame.sprite.Sprite):
 	def __init__(self,gs=None):
-		#Initialize the sprite, sound, and images:
+		#Initialize the sprite, and images:
 		pygame.sprite.Sprite.__init__(self)
 		self.image = pygame.image.load("images/redBlock2.png")
 		self.gs = gs
@@ -109,14 +126,19 @@ class Player(pygame.sprite.Sprite):
 		self.right = 0
 		self.up = 0
 		self.down = 0
+		self.canJump = 1
 
-		#Location/velocity of the death star:
+		#Location/velocity of the red player:
 		self.location = self.rect
 		self.gravity = 1
 		self.velX = 0
 		self.velY = 0
+
+
+	#If you jump, just set your velocity to be 20 upwards
 	def jump(self):
 		self.velY = -20
+		self.canJump = 0
 
 	def containWithinBorder(self):
 		l = self.rect
@@ -128,8 +150,8 @@ class Player(pygame.sprite.Sprite):
 			l.y = SCREEN_HEIGHT-self.rect.h
 		if l.x >= SCREEN_WIDTH-self.rect.w:
 			l.x = SCREEN_WIDTH-self.rect.w
-		# If you collide with a wall, move out based on velocity
 
+	#Move through all of the walls and platforms and check if you collide with any of them
 	def collideWalls(self):
 		for wall in gs.wall_list:
 		    if self.rect.colliderect(wall.rect):	
@@ -139,6 +161,7 @@ class Player(pygame.sprite.Sprite):
 					self.rect.top = wall.rect.bottom+1
 				else:
 					self.rect.bottom = wall.rect.top-1
+					self.canJump = 1
 				self.velY = 0
 			elif wall.type == 1:
 				self.rect.left = wall.rect.right+1
@@ -150,7 +173,10 @@ class Player(pygame.sprite.Sprite):
 			elif wall.type == 4:
 				self.rect.bottom = wall.rect.top-1
 				self.velY = 0
+				self.canJump = 1
 
+
+	#Move through all of the bullets and check if you collide with any of them
 	def collideBullets(self):
 		for b in gs.bullet_list:
 			if self.rect.colliderect(b.rect):
@@ -167,6 +193,8 @@ class Player(pygame.sprite.Sprite):
 			
 	#Tick (alters movement and rotates the player)
 	def tick(self):
+
+		#Reload delay (every 100 you get 1 bullet)
 		self.reloadDelay = self.reloadDelay+1
 		if self.reloadDelay == 100:
 			self.bullets = self.bullets +1
@@ -186,8 +214,12 @@ class Player(pygame.sprite.Sprite):
 						self.up = 1
 					elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
 						self.down = 1
+					#Jump:
 					elif event.key == pygame.K_SPACE:
-						self.jump()
+						if self.canJump:
+							self.jump()
+
+					#Shoot a bullet (if you have any):
 					elif event.key == pygame.K_c:
 						if self.bullets >0:
 							self.bullets = self.bullets -1
@@ -232,6 +264,8 @@ class Player(pygame.sprite.Sprite):
 			self.collideWalls()
 			self.collideBullets()
 
+
+		#If you are dead, send that info to the other player
 		if self.health == 0:
 			dict = {}
 			dict['message'] = "deletePlayer"
@@ -268,24 +302,22 @@ class GameSpace:
 		for x in range (0, 40):
 			if x > 20 and x < 30:
 				continue
-			self.wall_list.append(Wall(self, 10*x, 100,5)) 
-			self.wall_list.append(Wall(self, 10*x+400, 200,5)) 
-			self.wall_list.append(Wall(self, 10*x, 350,5))
-			self.wall_list.append(Wall(self, 10*x+400, 450,5))  
+			self.wall_list.append(Wall(self, 10*x, 100,5,1)) 
+			self.wall_list.append(Wall(self, 10*x+400, 200,5,0)) 
+			self.wall_list.append(Wall(self, 10*x, 350,5,1))
+			self.wall_list.append(Wall(self, 10*x+400, 450,5,0))  
 		for x in range (0, 30):
-			self.wall_list.append(Wall(self, 10*x, 700,5)) 
-			self.wall_list.append(Wall(self, 10*x+410, 600,5))
+			self.wall_list.append(Wall(self, 10*x, 700,5,1)) 
+			self.wall_list.append(Wall(self, 10*x+410, 600,5,0))
 		self.bullet_list = []
 
-
+	#This is the main loop for the game
 	def pygame_interior(self):
 		self.clock.tick(60)
 		self.player.tick()
 		if self.connected:
 			p = pickle.dumps(self.player.rect)
 			df.getProt().sendData(p)
-		
-		
 		for bullet in self.bullet_list:
 			bullet.tick()
 		self.screen.fill(self.black)
@@ -294,26 +326,30 @@ class GameSpace:
 			self.screen.blit(self.enemy.image,self.enemy.rect)
 		else:
 			self.screen.blit(self.redWinsImage,self.redWinsRect)
+			self.screen.blit(self.player.image,self.player.rect)
 		if self.player.health >0:
 			self.screen.blit(self.player.image,self.player.rect)
 		else:
 			self.screen.blit(self.blueWinsImage,self.blueWinsRect)
+			self.screen.blit(self.enemy.image,self.enemy.rect)
 
 		for bullet in self.bullet_list:
 			self.screen.blit(bullet.image, bullet.rect)
 		for wall in self.wall_list:
+			wall.tick()
 			self.screen.blit(wall.image, wall.rect)
 		pygame.display.flip()
+
+
+
+	#Functions for handling results from network messages
 	def areConnected(self):
 		self.connected = 1
-
 	def moveEnemy(self,rect):
 		self.enemy.rect = rect
-
 	def makeBullet(self,bdict):
 		b = Bullet(self,bdict['x'],bdict['y'],bdict['facing'])
 		self.bullet_list.append(b)
-	
 	def deleteBullet(self,b_x):
 		for b in self.bullet_list:
 			if abs(b.rect.x-b_x)<100:
